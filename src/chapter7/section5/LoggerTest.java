0ce22c3af31b1a36fc626b91e07c05f25dc407f8
Spring_Zhang <457540867@qq.com>
2019-11-27 21:56:15 +0800

    11-27

--- src/chapter7/section5/LoggerTest.java
+++ src/chapter7/section5/LoggerTest.java
@@ -1,5 +1,7 @@
 package chapter7.section5;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -8,11 +10,21 @@ import java.util.logging.Logger;
  * @date 2019/11/26 13:32
  */
 public class LoggerTest {
-    private static final Logger myLogger = Logger.getLogger("chapter7.section5");
+    /**
+     * 在请求日志记录器时，可以指定资源包
+     */
+    private static final Logger myLogger = Logger.getLogger("loggerName", "chapter7.section5.logmessages");
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Logger.getGlobal().info("File->Open menu item selected");
-        Logger.getGlobal().setLevel(Level.OFF);
-        Logger.getGlobal().info("File");
+        Logger.getGlobal().fine("File");
+        // 为日志指定资源包的关键字，而不是实际的日志消息字符串
+        myLogger.info("readingFile");
+        // 通常需要在本地化的消息中增加一些参数，可以包含占位符｛0｝，｛1｝等
+        myLogger.log(Level.INFO, "moreMessage", new Object[] { "的", "." });
+
+        FileHandler handler = new FileHandler();
+        myLogger.addHandler(handler);
+        myLogger.info("readingFile");
     }
 }
