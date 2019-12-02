8f8d564480a911343480f3384d54f2b8acfc42ce
Spring_Zhang <457540867@qq.com>
2019-12-02 15:54:26 +0800

    12-01

--- src/chapter8/section2/PairTest1.java
+++ src/chapter8/section2/PairTest1.java
@@ -6,6 +6,9 @@ public class PairTest1 {
         Pair<String> mm = ArrayAlg.minmax(words);
         System.out.println("最小值：" + mm.getFirst());
         System.out.println("最大值：" + mm.getSecond());
+
+        String middle = ArrayAlg.<String>getMiddle("John", "Q", "public");
+        System.out.println(middle);
     }
 }
 
@@ -25,5 +28,22 @@ class ArrayAlg {
             }
         }
         return new Pair<>(min, max);
+    }
+
+    public static <T> T getMiddle(T... a) {
+        return a[a.length / 2];
+    }
+
+    public static <T extends Comparable> T min(T[] a) {
+        if (a == null || a.length == 0) {
+            return null;
+        }
+        T smallest = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if (smallest.compareTo(a[i]) > 0) {
+                smallest = a[i];
+            }
+        }
+        return smallest;
     }
 }
