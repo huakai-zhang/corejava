da55cc91abf4f932f212528a2b46df8275d8366b
Spring_Zhang <457540867@qq.com>
2019-12-03 19:39:56 +0800

    12-03 19:39

---
+++ src/chapter9/QueueTest.java
@@ -0,0 +1,10 @@
+package chapter9;
+
+import java.util.Queue;
+
+public class QueueTest {
+    public static void main(String[] args) {
+        /*Queue<Customer> expressLane = new CircularArrayQueue<>(100);
+        expressLane.add(new Customer("Harry"));*/
+    }
+}
