503e733b80e86219b74ce00060ad4434dd41b4db
SpringZhang2018 <457540867@qq.com>
2019-12-18 18:07:10 +0800

    12-18 18：07

---
+++ src/chapter11/section10/GroupTest.java
@@ -0,0 +1,27 @@
+package chapter11.section10;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author Spring Zhang
+ * @date 2019/12/18 9:11
+ */
+public class GroupTest {
+    public static void main(String[] args) {
+        Stream<Locale> locales = Stream.of(Locale.getAvailableLocales());
+        /*Map<String, List<Locale>> countryToLocales = locales.collect(
+                Collectors.groupingBy(Locale::getCountry));
+        List<Locale> swissLocales = countryToLocales.get("CN");
+        // [zh_CN]
+        System.out.println(swissLocales);*/
+
+        Map<Boolean, List<Locale>> englishAndOtherLocales = locales.collect(
+                Collectors.partitioningBy(l -> l.getLanguage().equals("en")));
+        List<Locale> englishLocales = englishAndOtherLocales.get(true);
+        System.out.println(englishLocales);
+    }
+}
---
+++ src/chapter11/section13/IntStreamTest.java
@@ -0,0 +1,28 @@
+package chapter11.section13;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * @author Spring Zhang
+ * @date 2019/12/18 17:07
+ */
+public class IntStreamTest {
+    public static void main(String[] args) {
+        int[] values = {1, 1, 2, 3, 5};
+        IntStream stream = IntStream.of(1, 1, 2, 3, 5);
+        stream = Arrays.stream(values, 2, 4);
+        stream.forEach(System.out::println);
+        IntStream zeroToNinetyNine = IntStream.range(0 ,100);
+        IntStream zeroToHundred = IntStream.rangeClosed(0 ,100);
+        //zeroToNinetyNine.forEach(System.out::println);
+        //zeroToHundred.forEach(System.out::println);
+
+        String sentence = "\uD835\uDD46 is the set of octonions.";
+        IntStream codes = sentence.codePoints();
+        codes.forEach(System.out::println);
+
+        Stream<Integer> integers = IntStream.range(0, 100).boxed();
+    }
+}
