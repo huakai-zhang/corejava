a7fb24f29d68015248ee7038040ee8a49782b4ea
SpringZhang2018 <457540867@qq.com>
2019-12-19 18:14:43 +0800

    12-19 18：14

---
+++ src/chapter12/section1/OutputStream.java
@@ -0,0 +1,23 @@
+package chapter12.section1;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * @author Spring Zhang
+ * @date 2019/12/19 13:52
+ */
+public class OutputStream {
+    public static void main(String args[]){
+        try{
+            byte[] bytes = {122,104,97,110,103};
+            FileOutputStream out = new FileOutputStream("E:/Java/IdeaProjects/corejava/src/chapter12/section1/2.txt");
+            out.write(bytes);
+            out.close();
+        } catch (IOException e1){
+            e1.printStackTrace();
+        }
+    }
+}
