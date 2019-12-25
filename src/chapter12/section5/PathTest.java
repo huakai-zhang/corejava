c9c66873da8b21e2d2fe3dde914261e87be6929f
SpringZhang2018 <457540867@qq.com>
2019-12-25 18:00:28 +0800

    12-25 18:00

--- src/chapter12/section5/PathTest.java
+++ src/chapter12/section5/PathTest.java
@@ -5,14 +5,28 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Properties;
 
 public class PathTest {
     public static void main(String[] args) throws IOException {
-        Path absolute = Paths.get("D:\\", "test", "test2.txt");
+        /*Path absolute = Paths.get("D:\\", "test", "test2.txt");
         Path relative = Paths.get("test", "test.txt");
         List<String> lines = Files.readAllLines(absolute);
         System.out.println(lines);
         lines = Files.readAllLines(relative);
-        System.out.println(lines);
+        System.out.println(lines);*/
+
+        /*Properties props = new Properties();
+        String baseDir = props.getProperty("base.dir");
+        Path basePath = Paths.get(baseDir);*/
+
+        Path basePath = Paths.get("E:\\JAVA\\IdeaProjects\\corejava\\src");
+        Path workRelative = Paths.get("chapter3");
+        Path chapter3 = basePath.resolve(workRelative);
+        Path chapter4 = chapter3.resolveSibling("chapter4");
+        System.out.println(chapter3.toString());
+        System.out.println(chapter4.toString());
+        //E:\JAVA\IdeaProjects\corejava\src\chapter3
+        //E:\JAVA\IdeaProjects\corejava\src\chapter4
     }
 }
