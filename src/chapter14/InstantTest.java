814e7843379c48810d796e9347d8bac01d9ff2fb
SpringZhang2018 <457540867@qq.com>
2020-01-02 18:01:13 +0800

    1-2 18：01

---
+++ src/chapter14/InstantTest.java
@@ -0,0 +1,14 @@
+package chapter14;
+
+import java.time.Instant;
+
+/**
+ * @author Spring Zhang
+ * @date 2020/1/2 11:47
+ */
+public class InstantTest {
+    public static void main(String[] args) {
+        Instant start = Instant.now();
+
+    }
+}
---
+++ src/chapter14/LocalTimeTest.java
@@ -0,0 +1,19 @@
+package chapter14;
+
+import java.time.LocalTime;
+
+/**
+ * @author Spring Zhang
+ * @date 2020/1/2 14:09
+ */
+public class LocalTimeTest {
+    public static void main(String[] args) {
+        LocalTime rightNow = LocalTime.now();
+        LocalTime bedtime = LocalTime.of(22, 30);
+        // plus和minus按照一天24小时操作
+        LocalTime wakeup = bedtime.plusHours(8);
+        System.out.println(rightNow);
+        System.out.println(bedtime);
+        System.out.println(wakeup);
+    }
+}
