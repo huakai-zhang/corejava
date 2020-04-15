73644e2241f862a1a218f963006706f87833e77a
SpringZhang2018 <457540867@qq.com>
2020-04-15 18:09:50 +0800

    4-15 18:09

--- dubbo-order/order-provider/src/main/java/com/spring/dubbo/order/OrderServiceImpl2.java
+++ dubbo-order/order-provider/src/main/java/com/spring/dubbo/order/OrderServiceImpl2.java
@@ -6,13 +6,13 @@ import org.springframework.stereotype.Service;
  * @author Spring 花开不合阳春暮
  * @since 2020/4/14
  */
-@Service(value = "orderService")
-public class OrderServiceImpl implements IOrderService {
+@Service(value = "orderService2")
+public class OrderServiceImpl2 implements IOrderService {
     @Override
     public DoOrderResponse doOrder(DoOrderRequest request) {
-        System.out.println("曾经来过：" + request);
+        System.out.println("曾经来过2.0：" + request);
         DoOrderResponse response = new DoOrderResponse();
-        response.setCode("1000");
+        response.setCode("11111");
         response.setMemo("处理成功");
         return response;
     }
