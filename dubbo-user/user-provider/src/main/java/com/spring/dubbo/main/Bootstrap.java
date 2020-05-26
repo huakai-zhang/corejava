d33c7adb76516799243787175abc273f7296c858
SpringZhang2018 <457540867@qq.com>
2020-05-26 21:32:36 +0800

    5-26

--- dubbo-user/user-provider/src/main/java/com/spring/dubbo/main/Bootstrap.java
+++ dubbo-user/user-provider/src/main/java/com/spring/dubbo/main/Bootstrap.java
@@ -1,6 +1,8 @@
 package com.spring.dubbo.main;
 
+import com.alibaba.dubbo.common.extension.ExtensionLoader;
 import com.alibaba.dubbo.container.Main;
+import com.alibaba.dubbo.rpc.Protocol;
 
 /**
  * @author Spring 花开不合阳春暮
@@ -9,5 +11,7 @@ import com.alibaba.dubbo.container.Main;
 public class Bootstrap {
     public static void main(String[] args) {
         Main.main(args);
+        //Protocol protocol = ExtensionLoader.getExtensionLoader(Protocol.class).getExtension("myprotocol");
+        //System.out.println(protocol.getDefaultPort());
     }
 }
--- java/corejava/src/main/java/Test.java
+++ java/corejava/src/main/java/Test.java
@@ -2,15 +2,46 @@ import org.apache.ibatis.io.Resources;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author Spring Zhang
  * @date 2019/12/16 13:20
  */
 public class Test {
-
+    public final static String SDF_TIME = "yyyy-MM-dd HH:mm:ss";
     public static void main(String[] args) throws IOException {
-        InputStream inputStream= Resources.getResourceAsStream("mybatis-config.xml");
-        System.out.println(inputStream.read());
+        Date beginDate = formatDate("2020-05-25 16:35:21");
+        Date endDate = formatDate("2020-05-25 16:38:22");
+        /*InputStream inputStream= Resources.getResourceAsStream("mybatis-config.xml");
+        System.out.println(inputStream.read());*/
+
+        SimpleDateFormat format = new SimpleDateFormat(SDF_TIME);
+        String beginDateStr = null;
+        String endDateStr = null;
+
+        try {
+            beginDateStr = format.format(beginDate);
+            endDateStr = format.format(endDate);
+            beginDate = format.parse(beginDateStr);
+            endDate = format.parse(endDateStr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println((endDate.getTime() - beginDate.getTime()) / (60 * 1000));
+
+    }
+
+    public static Date formatDate(String date) {
+        DateFormat fmt = new SimpleDateFormat(SDF_TIME);
+        try {
+            return fmt.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
