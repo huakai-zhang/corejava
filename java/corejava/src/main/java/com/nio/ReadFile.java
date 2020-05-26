072b836e4211e92b47fb84ae51b141de657c2d75
张春阳 <457540867@qq.com>
2020-05-26 21:53:03 +0800

    5-26 21:53

---
+++ java/corejava/src/main/java/com/nio/ReadFile.java
@@ -0,0 +1,24 @@
+package com.nio;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+
+public class ReadFile {
+    public static void main(String[] args) throws Exception {
+        FileInputStream input = new FileInputStream("/Users/spring_zhang/Documents/info.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        String nameLine = reader.readLine();
+        String ageLine = reader.readLine();
+        String emailLine = reader.readLine();
+        String phoneLine = reader.readLine();
+        String lastLine = reader.readLine();
+        System.out.println(nameLine);
+        System.out.println(ageLine);
+        System.out.println(emailLine);
+        System.out.println(phoneLine);
+        System.out.println(lastLine);
+        input.close();
+    }
+}
