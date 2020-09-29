ebd9db6474e759a17678d9177302e787f7a1acd1
张春阳 <457540867@qq.com>
2020-09-29 21:28:43 +0800

    java引用类型2

---
+++ java/corejava/src/main/java/com/jvm/WeakHashMapDemo.java
@@ -0,0 +1,42 @@
+package com.jvm;
+
+import java.util.HashMap;
+import java.util.WeakHashMap;
+
+public class WeakHashMapDemo {
+    public static void main(String[] args) {
+        myHashMap();
+        myWeakHashMap();
+    }
+
+    private static void myWeakHashMap() {
+        WeakHashMap<Integer, String> map = new WeakHashMap<>();
+        Integer key = new Integer(2);
+        String value = "WeakHashMap";
+
+        map.put(key, value);
+        System.out.println(map);
+
+        // 此处如果不设置为null，则不会被回收
+        key = null;
+        System.out.println(map);
+
+        System.gc();
+        System.out.println(map + "\t" + map.size());
+    }
+
+    private static void myHashMap() {
+        HashMap<Integer, String> map = new HashMap<>();
+        Integer key = new Integer(1);
+        String value = "HashMap";
+
+        map.put(key, value);
+        System.out.println(map);
+
+        key = null;
+        System.out.println(map);
+
+        System.gc();
+        System.out.println(map + "\t" + map.size());
+    }
+}
