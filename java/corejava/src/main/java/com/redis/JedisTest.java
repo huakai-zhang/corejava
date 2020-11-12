a88e36ec0022216b0eb4db808d802d5c2f198726
P200000388\Administrator <457540867@qq.com>
2020-11-12 19:08:35 +0800

    1

--- java/corejava/src/main/java/com/redis/JedisTest.java
+++ java/corejava/src/main/java/com/redis/JedisTest.java
@@ -4,7 +4,7 @@ import redis.clients.jedis.Jedis;
 
 public class JedisTest {
     public static void main(String[] args) {
-        Jedis jedis = new Jedis("192.168.25.128", 6379);
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
         jedis.auth("1234");
         jedis.set("spring", "666");
         System.out.println(jedis.get("spring"));
