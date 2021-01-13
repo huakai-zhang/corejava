c6d07ff6f666e2b23c4eb97e6265466301f5a1ad
张春阳 <457540867@qq.com>
2021-01-13 21:48:48 +0800

    no message

---
+++ springcloud-demo/config-server/src/main/java/com/spring/Application.java
@@ -0,0 +1,19 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author 花开不合阳春暮
+ * @date 2021/1/13 下午9:43
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
