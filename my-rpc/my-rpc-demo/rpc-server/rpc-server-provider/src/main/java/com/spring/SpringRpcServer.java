53265b42b3c9880227e646b4e95757c2dc8c5c49
P200000388\Administrator <457540867@qq.com>
2021-02-02 19:37:38 +0800

    no message

--- my-rpc/my-rpc-demo/rpc-server/rpc-server-provider/src/main/java/com/spring/SpringRpcServer.java
+++ my-rpc/my-rpc-demo/rpc-server/rpc-server-provider/src/main/java/com/spring/SpringRpcServer.java
@@ -32,6 +32,9 @@ public class SpringRpcServer implements ApplicationContextAware, InitializingBea
         this.port = port;
     }
 
+    /**
+     * InitializingBean 为 bean 提供了初始化方法的方式，它只包括 afterPropertiesSet 方法，凡是继承该接口的类，在初始化 bean 的时候会执行该方法
+     */
     @Override
     public void afterPropertiesSet() throws Exception {
         ServerSocket serverSocket = null;
@@ -54,6 +57,9 @@ public class SpringRpcServer implements ApplicationContextAware, InitializingBea
         }
     }
 
+    /**
+     * 实现了 ApplicationContextAware 接口的 bean，当 spring 容器初始化的时候，会自动的将 ApplicationContext 注入进来
+     */
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         Map<String, Object> serviceBeanMap = applicationContext.getBeansWithAnnotation(RpcService.class);
@@ -61,7 +67,8 @@ public class SpringRpcServer implements ApplicationContextAware, InitializingBea
             for(Object serviceBean : serviceBeanMap.values()) {
                 RpcService rpcService = serviceBean.getClass().getAnnotation(RpcService.class);
                 String serviceName = rpcService.value().getName();
-                String version = rpcService.version(); //拿到版本号
+                // 拿到版本号
+                String version = rpcService.version();
                 if(!StringUtils.isEmpty(version)) {
                     serviceName += "-" + version;
                 }
