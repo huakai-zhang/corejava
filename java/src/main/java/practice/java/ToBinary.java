e90bb3be1f478833295eb3df2701bc63d456481b
P200000388\Administrator <457540867@qq.com>
2021-03-15 18:52:26 +0800

    32位十进制转二进制

---
+++ java/src/main/java/practice/java/ToBinary.java
@@ -0,0 +1,21 @@
+package practice.java;
+
+/**
+ * @author 春阳
+ * @date 2021-03-15 18:13
+ */
+public class ToBinary {
+    public static void main(String[] args) {
+        int n = -10;
+        for (int i = 31; i >= 0; i--) {
+            if ((n & (1 << i)) != 0) {
+                System.out.print("1");
+            } else {
+                System.out.print("0");
+            }
+            if ((32 - i) % 4 == 0) {
+                System.out.print(" ");
+            }
+        }
+    }
+}
