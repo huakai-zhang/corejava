e327c8fd53e4a3e71c9770cc7a71638e98537328
P200000388\Administrator <457540867@qq.com>
2021-04-25 19:31:19 +0800

    no message

--- java/src/main/java/com/juc/volatiles/UseVolatile02.java
+++ java/src/main/java/com/juc/volatiles/UseVolatile02.java
@@ -1,5 +1,7 @@
 package com.juc.volatiles;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author Spring 花开不合阳春暮
  * @since 2020/9/17
@@ -9,7 +11,7 @@ public class UseVolatile02 {
     int a = 1;
     int b = 2;
     int c = 3;
-    volatile boolean flag = false;
+    boolean flag = false;
 
     public void write() {
         a = 100;
@@ -18,20 +20,32 @@ public class UseVolatile02 {
         flag = true;
     }
 
-    public void read() {
+    public void read() throws InterruptedException {
+        while (!flag) {
+            System.out.println("flag = " + flag + " 还未触发");
+            TimeUnit.MILLISECONDS.sleep(100);
+        }
         while (flag) {
             // flag被volatile修饰，充当了触发器，一旦值为true,此处立即对变量之前的操作可见。
-            System.out.println("a = " + a + ",b = " + b + ", c = " + c);
+            System.out.println("flag = " + flag + " 触发, a = " + a + ",b = " + b + ", c = " + c);
+            TimeUnit.MILLISECONDS.sleep(100);
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
         UseVolatile02 useVolatile02 = new UseVolatile02();
         new Thread(() -> {
-            useVolatile02.write();
+            try {
+                useVolatile02.read();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }).start();
+
+        TimeUnit.SECONDS.sleep(1);
+
         new Thread(() -> {
-            useVolatile02.read();
+            useVolatile02.write();
         }).start();
     }
 }
